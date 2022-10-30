923e224ec3301bb4ecc2222f57cd8e1d650a93fd
Mathew Chan <mchan6466@gmail.com>
2022-10-31 01:08:44 +0900

    add tap to expand dictionary definition

--- lib/widgets/reader/vocabulary_definition.dart
+++ lib/widgets/reader/vocabulary_definition.dart
@@ -2,23 +2,45 @@ import 'package:flutter/cupertino.dart';
 import 'package:immersion_reader/dictionary/dictionary_entry.dart';
 import 'package:immersion_reader/japanese/vocabulary.dart';
 
-class VocabularyDefinition extends StatelessWidget {
+class VocabularyDefinition extends StatefulWidget {
   final Vocabulary vocabulary;
-
   const VocabularyDefinition({super.key, required this.vocabulary});
+
+  @override
+  State<VocabularyDefinition> createState() => _VocabularyDefinitionState();
+}
+
+class _VocabularyDefinitionState extends State<VocabularyDefinition> {
+  Map<DictionaryEntry, bool> definitionsExpanded = {};
+
+  @override
+  void initState() {
+    super.initState();
+    for (DictionaryEntry entry in widget.vocabulary.entries) {
+      definitionsExpanded[entry] = false;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(children: [
-      ...vocabulary.entries
-          .map((DictionaryEntry entry) => Align(
-              alignment: Alignment.centerLeft,
-              child: Text(entry.meanings.join('; '),
-                  textAlign: TextAlign.left,
-                  style: const TextStyle(
-                      color: CupertinoColors.inactiveGray,
-                      fontSize: 15,
-                      overflow: TextOverflow.ellipsis))))
+      ...widget.vocabulary.entries
+          .map((DictionaryEntry entry) => GestureDetector(
+              onTap: () {
+                setState(() {
+                  definitionsExpanded[entry] = !definitionsExpanded[entry]!;
+                });
+              },
+              child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(entry.meanings.join('; '),
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                          color: CupertinoColors.inactiveGray,
+                          fontSize: 15,
+                          overflow: definitionsExpanded[entry]!
+                              ? null
+                              : TextOverflow.ellipsis)))))
           .toList()
     ]);
   }
